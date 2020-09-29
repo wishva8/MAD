06ebfdc491fa08d5c04af80a82dbfb514ed02786
wishva perera <vibushitha99@gamil.com>
2020-09-30 00:53:42 +0530

    firebase 1

--- app/src/main/java/com/example/mad/MainActivity3.java
+++ app/src/main/java/com/example/mad/MainActivity3.java
@@ -3,14 +3,78 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    EditText txtName,txtdrivinLNO,address,tel,experience,qualification;
+    Button btnsave;
+    DatabaseReference dbRef;
+    Rider rider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-    }
 
+        txtName=findViewById(R.id.etName);
+        txtdrivinLNO=findViewById(R.id.etDrivingLNo);
+        address =findViewById(R.id.etAddress);
+        tel=findViewById(R.id.etTel);
+        experience=findViewById(R.id.etExperience);
+        qualification=findViewById(R.id.etQualification);
+        btnsave=findViewById(R.id.btnsave);
+
+        rider=new Rider();
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Rider");
+
+                if (TextUtils.isEmpty(txtName.getText().toString()))
+                    Toast.makeText(MainActivity3.this, "Enter Name", Toast.LENGTH_SHORT).show();
+                else if (TextUtils.isEmpty(txtdrivinLNO.getText().toString()))
+                    Toast.makeText(MainActivity3.this, "Enter Driving License No", Toast.LENGTH_SHORT).show();
+                else if (TextUtils.isEmpty(address.getText().toString()))
+                    Toast.makeText(MainActivity3.this, "Enter Address", Toast.LENGTH_SHORT).show();
+                else if (TextUtils.isEmpty(tel.getText().toString()))
+                    Toast.makeText(MainActivity3.this, "Enter Telephone Number", Toast.LENGTH_SHORT).show();
+                else if (TextUtils.isEmpty(experience.getText().toString()))
+                    Toast.makeText(MainActivity3.this, "Enter Experience", Toast.LENGTH_SHORT).show();
+                else if (TextUtils.isEmpty(qualification.getText().toString()))
+                    Toast.makeText(MainActivity3.this, "Enter Qualification", Toast.LENGTH_SHORT).show();
+                else {
+
+                    rider.setName(txtName.getText().toString().trim());
+                    rider.setDrivinLNO(txtdrivinLNO.getText().toString().trim());
+                    rider.setAddress(address.getText().toString().trim());
+                    rider.setTel(tel.getText().toString().trim());
+                    rider.setExperience(experience.getText().toString().trim());
+                    rider.setQualification(qualification.getText().toString().trim());
+                    dbRef.child(txtdrivinLNO.getText().toString()).setValue(rider);
+                    Toast.makeText(MainActivity3.this, "Successfully Inserted", Toast.LENGTH_SHORT).show();
+                    Clear();
+                }
+            }
+        } );
+
+
+    }
+    private void Clear(){
+        txtName.setText("");
+        txtdrivinLNO.setText("");
+        address.setText("");
+        tel.setText("");
+        experience.setText("");
+        qualification.setText("");
+    }
 
 }
--- app/src/main/java/com/example/mad/Rider.java
+++ app/src/main/java/com/example/mad/Rider.java
@@ -1,4 +1,61 @@
 package com.example.mad;
 
 public class Rider {
+    private String name;
+    private String drivinLNO;
+    private String address;
+    private String tel;
+    private String experience;
+    private String qualification;
+
+    public Rider() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDrivinLNO() {
+        return drivinLNO;
+    }
+
+    public void setDrivinLNO(String drivinLNO) {
+        this.drivinLNO = drivinLNO;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
 }
